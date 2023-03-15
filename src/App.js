6093f83f77d9008996cd4d28a3615e2bae7f99fa
Abhinav Khanna <c0873411@mylambton.ca>
2023-03-15 16:34:41 -0400

    router setup

--- src/App.js
+++ src/App.js
@@ -5,26 +5,38 @@ import axios from 'axios';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert'
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import About from './components/pages/About';
+import User from './components/users/User';
 
 class App extends Component {
     state = {
         users: [],
+        user: {},
         loading: false,
         alert: null
     }
-
+    
+    // Search all users
     searchUsers = async (text) => {
         this.setState({ loading: true });
         const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
         this.setState({ users: res.data.items, loading: false });
     }
 
+    // Search specific user
+    getUser = async (username) => {
+        this.setState({ loading: true });
+        const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+        this.setState({ user: res.data, loading: false });
+    }
+
+    // Clear data from page
     clearData = (text) => {
         this.setState({ users: [], loading: false });
     }
 
+    // Alert popup from 
     setAlert = (msg, type) => {
         this.setState({ alert: { msg: msg, type: type } });
         setTimeout(() => this.setState({ alert: null }), 3000);
@@ -37,20 +49,21 @@ class App extends Component {
                     <Navbar title="Github" github_icon="fab fa-github" ></Navbar>
                     <div className="container">
                         <Alert alert={this.state.alert}></Alert>
-                        <Switch>
-                            <Route exact path="/" render={(props) => {
-                                return <Fragment>
-                                    <Search searchUsers={this.searchUsers} setAlert={this.setAlert} clearData={this.clearData} showClear={this.state.users.length > 0 ? true : false}></Search>
-                                    <Users loading={this.state.loading} users={this.state.users}></Users>
+                        <Routes>
+                            <Route exact path="/" element={
+                                <Fragment>
+                                    <Search searchUsers={this.searchUsers} setAlert={this.setAlert} clearData={this.clearData} showClear={this.state.users.length > 0 ? true : false} ></Search>
+                                    <Users loading={this.state.loading} users={this.state.users} ></Users>
                                 </Fragment>
-                            }}>
-
+                            }>
                             </Route>
-                            <Route exact path='/about' component={About}>
 
+                            <Route exact path="/about" element={<About/>}>
                             </Route>
-                        </Switch>
 
+                            <Route exact path="/user/:login" element={ <User getUser={this.getUser} user={this.state.user} loading={this.state.loading} /> }>
+                            </Route>
+                        </Routes>
                     </div>
                 </div>
             </Router>
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = (props) => {
                 <ul>
                     <li><Link to="/" >Home</Link></li>
                     <li><Link to="/about" >About</Link></li>
-                    <li><Link to="/contact-us" >Contact Us</Link></li>
+                    {/* <li><Link to="/contact-us" >Contact Us</Link></li> */}
                 </ul>
             </nav>
         )
